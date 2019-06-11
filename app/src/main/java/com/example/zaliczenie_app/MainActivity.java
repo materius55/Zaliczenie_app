1965f26c60a661d741bae36ecb68f3bd9551f2f2
materius55 <mateusz.wisniewski04@student.wat.edu.pl>
2019-06-11 21:45:14 +0200

    v.0.2
    Dodanie funkcji generowania losowych liczb, podstawowy layout

--- app/src/main/java/com/example/zaliczenie_app/MainActivity.java
+++ app/src/main/java/com/example/zaliczenie_app/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.zaliczenie_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    int wylosowana;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Losowanie(View view) {
+        int wylosowana;
+        Random rand = new Random();
+
+        wylosowana = rand.nextInt((50-10)+1)+10;
+
+        TextView textView = (TextView)findViewById(R.id.textView2);
+        textView.setText(String.valueOf(wylosowana));
+
     }
 }
